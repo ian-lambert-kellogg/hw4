29e8229615651db17e05b4f07259bb30067373c7
Ian Lambert <ian.lambert@kellogg.northwestern.edu>
2025-03-15 03:45:38 +0000

    Updated the user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
+    @user["username"] = params["username"]
     @user["email"] = params["email"]
     @user["password"] = params["password"]
     @user.save
-    redirect_to "/user/#{@user["id"]}"
+    redirect_to "/users/#{@user["id"]}"
   end
 end
