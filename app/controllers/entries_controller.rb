621c097a4f6b3d61f12543c2295ebf1fef51d2eb
Ian Lambert <ian.lambert@kellogg.northwestern.edu>
2025-03-16 23:37:51 +0000

    got the images to stack

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,17 +1,26 @@
 class EntriesController < ApplicationController
 
   def new
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = session["user_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["image"] = params["image"]
+      @entry["user_id"] = session["user_id"]
+      @entry.save
+    else
+      flash["notice"] = "Login first."
+    end
+      redirect_to "/places/#{@entry["place_id"]}"
   end
 
 end
+
+
